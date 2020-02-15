a796984c1f1eba256428fe53d8cf886b86c6f043
Maxik-svg <60792664+Maxik-svg@users.noreply.github.com>
2020-02-15 23:50:47 +0200

    CreatingScene

---
+++ RTS/Assets/Scripts/CellScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellScript : MonoBehaviour
+{
+    public GameObject Window;
+    GameObject Canvas;
+    GameObject window;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Canvas = GameObject.FindGameObjectWithTag("Canvas");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ RTS/Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject Cell;
+    public GameObject Canvas;
+    Vector2[,] gameField; //array of all game cells
+    int n, m; //Height & width of our game field
+    
+    void Start()
+    {
+        Instantiate(Canvas);
+        (n, m) = (15, 15); 
+        (int N, int M) = ((int)Mathf.Floor((n/2)), (int)Mathf.Floor((m / 2))); //Will be used for changing positions of cells
+        gameField = new Vector2[n, m];
+
+
+        
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                gameField[i, j] = new Vector2(-M, N);
+                Cell.transform.position = gameField[i, j];
+                Instantiate(Cell);
+                M--;
+            }
+            N--;
+            M = (int)Mathf.Floor((m / 2));
+        }
+    }
+}
